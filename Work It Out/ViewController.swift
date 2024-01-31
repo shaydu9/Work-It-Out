16fc434797dd932a6f12f42be73a0f58d993bb13
Shay Dubrovsky <shaydu9@gmail.com>
2024-01-31 16:00:22 +0200

    Initial Commit

---
+++ Work It Out/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Work It Out
+//
+//  Created by Shay Dubrovsky on 31/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
